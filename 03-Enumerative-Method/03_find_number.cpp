bb16b4ef5693c873f997bb4e75f41cc3cd97cf16
shengchiliu <shengchiliu@cam.ac.uk>
2018-05-13 23:40:02 +0100

    modified

--- 03-Enumerative-Method/03_find_number.cpp
+++ 03-Enumerative-Method/03_find_number.cpp
@@ -1,3 +1,27 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+
+//     int array[7] = {2, 4, 1, 6, 12, 3, 7};
+
+//     bool find_number(int n) {
+
+//         bool found = false;
+
+//         for (int i=0; i<7; i++) {
+            
+//             if (array[i] == n) {
+//                 found = true;
+//                 break;
+//             }
+//         }
+//         return found;
+//     }
+// }
+
+
 #include <iostream>
 
 using namespace std;
@@ -8,15 +32,12 @@ int main() {
 
     bool find_number(int n) {
 
-        bool found = false;
-
         for (int i=0; i<7; i++) {
             
             if (array[i] == n) {
-                found = true;
-                break;
+                return true;
             }
         }
-        return found;
+        return false;
     }
 }
